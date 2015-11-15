12bc9688a7105cd37f4cb8edf535fc72e62f4c67
Matt Cameron <mcameron@avalanche.com.au>
2015-11-15 15:59:45 +1100

    Give the home page some style

--- views/home.js
+++ views/home.js
@@ -6,18 +6,20 @@ var ScheduleView = require('../views/schedule.js');
 var VoteView = require('../views/voteMain.js');
 var CameraView = require('../views/cameraPage.js');
 var NavBarButton = require('../components/NavBarButton');
+var Icon = require('react-native-vector-icons/FontAwesome');
 
-// var MyTopHeader = require('../components/header');
 var {
   AppRegistry,
   StyleSheet,
   Text,
   View,
-  Navigator
+  Navigator,
+  ListView,
+  TouchableHighlight
 } = React;
 
 var HomeView = React.createClass({
-    goToFeedView: function() {
+    goToFeed: function() {
       this.props.navigator.push({
             name: 'FeedView',
             component: FeedView,
@@ -26,7 +28,7 @@ var HomeView = React.createClass({
               component: NavBarButton,
               passProps: {
                 icon: 'awesome|camera',
-                onPress: this.goToCameraPage,
+                onPress: this.goToCamera,
               }
             },
         });
@@ -40,12 +42,12 @@ var HomeView = React.createClass({
               component: NavBarButton,
               passProps: {
                 icon: 'awesome|camera',
-                onPress: this.goToCameraPage,
+                onPress: this.goToCamera,
               }
             },
         });
     },
-    goToVotePage: function() {
+    goToVote: function() {
       this.props.navigator.push({
             name: 'VoteView',
             component: VoteView,
@@ -54,12 +56,12 @@ var HomeView = React.createClass({
               component: NavBarButton,
               passProps: {
                 icon: 'awesome|camera',
-                onPress: this.goToCameraPage,
+                onPress: this.goToCamera,
               }
             },
         });
     },
-    goToCameraPage: function() {
+    goToCamera: function() {
       this.props.navigator.push({
             name: 'CameraView',
             component: CameraView,
@@ -76,48 +78,94 @@ var HomeView = React.createClass({
     goHome: function() {
       this.props.navigator.popToTop();
     },
-    render: function() {
+    getInitialState: function() {
+      return {
+        dataSource: new ListView.DataSource({
+          rowHasChanged: (row1, row2) => row1 !== row2,
+        }),
+        loaded: false,
+        rowData: [
+          {
+            title: "Photos",
+            onPress: this.goToFeed,
+            backgroundColor: '#44E5E7',
+            icon: 'th'
+          },
+          {
+            title: "Schedule",
+            onPress: this.goToSchedule,
+            backgroundColor: '#41D39B',
+            icon: 'list'
+          },
+          {
+            title: "Camera",
+            onPress: this.goToCamera,
+            backgroundColor: '#F6AE2D',
+            icon: 'camera'
+          },
+          {
+            title: "Vote",
+            onPress: this.goToVote,
+            backgroundColor: '#59D2FE',
+            icon: 'trophy'
+          },
+
+        ]
+      };
+    },
+    componentDidMount: function() {
+      this.setState({
+        dataSource: this.state.dataSource.cloneWithRows(this.state.rowData)
+      });
+    },
+    renderRow: function(data) {
       return (
-        <View style={styles.container}>
-          <View style={styles.content}>
-            <Text>I am the Home Page</Text>
-            <Text onPress={this.goToFeedView}>
-              Feed View
-            </Text>
-            <Text onPress={this.goToSchedule}>
-              Schedule
-            </Text>
-            <Text onPress={this.goToVotePage}>
-              Vote
-            </Text>
-            <Text onPress={this.goToCameraPage}>
-              Camera
+        <View>
+          <TouchableHighlight onPress={data.onPress}>
+          <View style={[styles.row, {backgroundColor: data.backgroundColor}]}>
+            <View style={styles.icon}>
+              <Icon name={data.icon} size={20} color="#fff" />
+            </View>
+            <Text style={styles.content}>
+              {data.title}
             </Text>
           </View>
+          </TouchableHighlight>
+        </View>
+      );
+    },
+    render: function() {
+      return (
+        <View style={styles.links}>
+          <ListView
+            style={styles.list}
+            dataSource={this.state.dataSource}
+            renderRow={this.renderRow}
+          />
         </View>
       );
     }
 });
 
 var styles = StyleSheet.create({
-  container: {
+
+  links: {
     flex: 1,
+    marginTop: 64
   },
-  content: {
-    flex: 1,
-    backgroundColor: '#F5FCFF',
-    justifyContent: 'center',
+  row: {
+    flexDirection: 'row',
+    height: 64,
     alignItems: 'center',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  icon: {
+    margin: 30,
+    alignItems: 'center',
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  content: {
+    alignItems: 'center',
+    color: '#fff',
+    fontSize: 20
   },
 });
 
